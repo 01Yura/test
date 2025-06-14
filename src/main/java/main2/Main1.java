d82c6505ca56a888a62df068a6a425fe5f89424a
Yuri Primyshev <p020885@yandex.ru>
2025-06-13 23:53:34 -0400

    31 commit

--- src/main/java/main2/Main1.java
+++ src/main/java/main2/Main1.java
@@ -3,6 +3,6 @@ package main2;
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("Change first from feature 1");
-        System.out.println("Change second from feature 1");
+        System.out.println("------Change second from feature 2--------");
     }
 }
