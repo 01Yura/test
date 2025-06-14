92214ca5d7facc5304d5a4dc04c2f091243984c6
Yuri Primyshev <p020885@yandex.ru>
2025-06-13 23:55:27 -0400

    32 commit

--- src/main/java/main2/Main1.java
+++ src/main/java/main2/Main1.java
@@ -4,5 +4,6 @@ public class Main1 {
     public static void main(String[] args) {
         System.out.println("Change first from feature 1");
         System.out.println("Change second from feature 1");
+        System.out.println("Third second from feature 1");
     }
 }
