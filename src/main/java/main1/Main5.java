9266834e0933d3406b06a0e3a0513227e5582d83
Yuri Primyshev <p020885@yandex.ru>
2025-06-13 23:37:00 -0400

    24 commit

--- src/main/java/main1/Main5.java
+++ src/main/java/main1/Main5.java
@@ -3,5 +3,6 @@ package main1;
 public class Main5 {
     public static void main(String[] args) {
         System.out.println("first change");
+        System.out.println("second change");
     }
 }
