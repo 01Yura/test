e1f986e8951f6829722098829bacd642279f5855
Yura Primyshev <40532543+01Yura@users.noreply.github.com>
2025-06-13 23:45:17 -0400

    Feature/222 (#7)
    * 23 commit
    
    * 24 commit
    
    * 25 commit
    
    * 26 commit

--- src/main/java/main1/Main5.java
+++ src/main/java/main1/Main5.java
@@ -1,7 +1,9 @@
 package main1;
 
+
 public class Main5 {
     public static void main(String[] args) {
         System.out.println("first change");
+        System.out.println("third change in feature 222");
     }
 }
---
+++ src/main/java/main1/Main6.java
@@ -0,0 +1,7 @@
+package main1;
+
+public class Main6 {
+    public static void main(String[] args) {
+        System.out.println("first change in Main6");
+    }
+}
