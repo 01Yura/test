5b7b420a8fa74f21e9a501466846e453fd290017
Yura Primyshev <40532543+01Yura@users.noreply.github.com>
2025-06-14 00:00:28 -0400

    feature/2 (#9)
    * 28 commit
    
    * 29 commit
    
    * 30 commit
    
    * 31 commit

--- src/main/java/main2/Main1.java
+++ src/main/java/main2/Main1.java
@@ -3,7 +3,9 @@ package main2;
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("Change first from feature 1");
+        System.out.println("------Change second from feature 2--------");
         System.out.println("Change second from feature 1");
         System.out.println("Third second from feature 1");
+
     }
 }
